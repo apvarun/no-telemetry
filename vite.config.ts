718e1c5ef9821c25f112f0458a9959faee26080f
Varun A P <8411309+apvarun@users.noreply.github.com>
2026-08-10 09:23:06 +0200

    init project

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite-plus";
+
+export default defineConfig({
+  staged: {
+    "*": "vp check --fix",
+  },
+  pack: {
+    entry: ["src/index.ts", "src/cli.ts"],
+    dts: true,
+    exports: true,
+  },
+  lint: {
+    options: {
+      typeAware: true,
+      typeCheck: true,
+    },
+  },
+  fmt: {},
+});
